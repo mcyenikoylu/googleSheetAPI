58c1382ab5dc02985a249c87e8730040010f5f55
Mehmet Cenk Yenikoylu <mcyenikoylu@gmail.com>
2020-10-16 14:14:26 +0300

    sheet data update

--- sheet1.js
+++ sheet1.js
@@ -11,7 +11,7 @@ const client = new google.auth.JWT(
 client.authorize(function(err,tokens){
     if(err){
         console.log(err);
-        return;
+        //return;
     } else {
         console.log('Connect');
         gsrun(client);
@@ -22,8 +22,23 @@ async function gsrun(cl){
     const gsapi = google.sheets({version:'v4', auth: cl});
     const doc = {
         spreadsheetId: '1RcLADPxuonv-S5kK1551sqUfT4bfyXftRHe8UgTilX4',
-        range: 'Class Data!A2:F2'
+        range: 'Class Data!A2:F31'
     }
     let req = await gsapi.spreadsheets.values.get(doc);
-    console.log(req.data);
+    //console.log(req.data.values);
+    let reqArray = req.data.values;
+    let newReqArray = reqArray.map(function(r){
+        r.push(r[0]+'-'+r[1]);
+        return r;
+    });
+
+    const docUpdate = {
+        spreadsheetId: '1RcLADPxuonv-S5kK1551sqUfT4bfyXftRHe8UgTilX4',
+        range: 'Class Data!H2',
+        valueInputOption: 'USER_ENTERED',
+        resource: {values: newReqArray}
+    }
+    let res = await gsapi.spreadsheets.values.update(docUpdate);
+
+    console.log(res);
 }
